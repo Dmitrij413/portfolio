a4fb2718842bc0cf21093581a6fc5a460145f3c9
Dmitrij <dmitrij.khramov@bk.ru>
2023-04-22 12:51:56 +0400

    Changes to be committed: 	modified:   src/App.jsx 	new file:   src/components/project/Project.jsx 	new file:   src/helpers/projectList.jsx 	modified:   src/pages/Projects.jsx 	modified:   src/styles/main.css

---
+++ src/helpers/projectList.jsx
@@ -0,0 +1,48 @@
+import project01 from "./../img/projects/01.jpg"
+import project01Big from "./../img/projects/01-big.jpg"
+import project02 from "./../img/projects/02.jpg"
+import project02Big from "./../img/projects/02-big.jpg"
+import project03 from "./../img/projects/03.jpg"
+import project03Big from "./../img/projects/03-big.jpg"
+import project04 from "./../img/projects/04.jpg"
+import project04Big from "./../img/projects/04-big.jpg"
+import project05 from "./../img/projects/05.jpg"
+import project05Big from "./../img/projects/05-big.jpg"
+import project06 from "./../img/projects/06.jpg"
+import project06Big from "./../img/projects/06-big.jpg"
+
+const projects = [
+	{
+		title: 'Gaming streaming portal',
+		skills: 'React, Node.js',
+		img: project01,
+		gitHubLink: 'https://github.com'
+	},
+	{
+		title: 'Video service',
+		skills: 'React, PHP, MongoDB',
+		img: project02,
+		gitHubLink: 'https://github.com'
+	},
+	{
+		title: 'Video portal',
+		skills: 'Vue JS, Node.js, MongoDB',
+		img: project03,
+		gitHubLink: 'https://github.com'
+	},
+	{
+		title: 'Dating app',
+		skills: 'React Native',
+		img: project04
+	},
+	{
+		title: 'Landing',
+		skills: 'HTML, SCSS, JS',
+		img: project05
+	},
+	{
+		title: 'Gaming community',
+		skills: 'React, PHP, MySQL',
+		img: project06
+	},
+]
