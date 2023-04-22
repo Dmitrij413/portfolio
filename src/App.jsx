a77ea9278bd973575b2e51eb9b01ad671cad58d5
Dmitrij413 <dmitrij.khramov@bk.ru>
2023-04-22 20:44:50 +0400

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/btnGitHub/BtnGitHub.jsx 	renamed:    src/img/icons/gitHub-black.svg -> src/components/btnGitHub/gitHub-black.svg 	modified:   src/components/navbar/Navbar.jsx 	new file:   src/pages/Project.jsx 	modified:   src/pages/Projects.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import './styles/main.css'
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/footer'
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
+import Project from './pages/Project';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      <Projects />
-      {/* <Contacts /> */}
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/project' element={<Project />} />
+          <Route path='/Contacts' element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </Router>
+      
     </div>
   );
 }
