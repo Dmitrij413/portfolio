9906aa4f6a45757c3633a6d9012a72baa3788e54
Dmitrij <dmitrij.khramov@bk.ru>
2023-04-22 12:30:07 +0400

    Changes to be committed: 	modified:   public/index.html 	modified:   src/App.jsx 	new file:   src/pages/Contcts.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './styles/main.css'
 
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/footer'
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Contacts from './pages/Contcts';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <Home /> */}
-      <Projects />
+      {/* <Projects /> */}
+      <Contacts />
       <Footer />
     </div>
   );
